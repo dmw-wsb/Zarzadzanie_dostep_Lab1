10e3932e98f7d2a366b37fe07d7babc9baabd2ef
dmw-wsb <101039753+dmw-wsb@users.noreply.github.com>
2022-11-28 08:25:21 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,43 @@
+// Funkcja rekurencyjna
+const express = require("express");
+var path = require('path');
+
+const app = express();
+
+function authentication(req, res, next) {
+	var authheader = req.headers.authorization;
+	console.log(req.headers);
+
+	if (!authheader) {
+		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		return next(err)
+	}
+
+	var auth = new Buffer.from(authheader.split(' ')[1],
+	'base64').toString().split(':');
+	var user = auth[0];
+	var pass = auth[1];
+
+	if (user == 'student' && pass == 'student') {/*zmiana hasla i nazwy uzytkownika*/
+
+		// pozytywne uwierzytelniemie
+		next();
+	} else {
+		var err = new Error('You are not authenticated!');
+		res.setHeader('WWW-Authenticate', 'Basic');
+		err.status = 401;
+		return next(err);
+	}
+
+}
+
+// Pierwszy krok uwierzytelniania uzytkownikow
+app.use(authentication)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Ustaweinia lokalnego serwera w celu sprawdzenia aplikacji
+app.listen((3000), () => {
+	console.log("Server is Running");
+})
